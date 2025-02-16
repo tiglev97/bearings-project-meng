48cd64c313807fa6b7956fae05bea11af6747314
Tigran Levonyan <tigranlevonyan@gmail.com>
2025-02-16 15:38:45 -0500

    Text update
    Changed text and wording.

--- react/anomaly-detection/src/pages/1_FileUploaderPage.js
+++ react/anomaly-detection/src/pages/1_FileUploaderPage.js
@@ -17,7 +17,7 @@ function ConvertTimeSeries() {
 
       {/* Step 1: File Upload */}
       <div>
-        <h1 style={{ fontSize: '50px', paddingTop: '50px' , textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Step 1: Upload and Validate Data</h1>
+        <h1 style={{ fontSize: '50px', paddingTop: '50px' , textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Upload and Validate Data</h1>
         <p style={{ fontSize: '18px', paddingBottom: '15px' }}>Upload your time series dataset in a valid ZIP format. The system will automatically validate the data for anomalies or errors.</p>
         <FileUpload />
       </div>
